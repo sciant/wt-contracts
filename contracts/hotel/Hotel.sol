4abe743a093bbbd2ad95610328bd4d2ef42480c9
Georgi Yovchev <georgi.yovchev@sciant.com>
2018-10-25 13:56:17 +0300

    Added private offers to hotel contract

--- contracts/hotel/Hotel.sol
+++ contracts/hotel/Hotel.sol
@@ -11,13 +11,37 @@ contract Hotel is AbstractHotel {
 
   bytes32 public contractType = bytes32("hotel");
 
+  enum BookingRequestStatus {PENDING,ACCEPTED,REJECTED}
+
+  struct BookingRequest{
+    address sender;
+    string encryptedData;
+    uint256[] cancellationFrom;
+    uint256[] cancellationTo;
+    uint8[] cancellationAmount;
+    uint256 total;
+    BookingRequestStatus status;
+  }
+  // List of all booking requests
+  BookingRequest[] bookingRequests;
+
+
+  event newBookingRequest(address _sender, string _encryptedData, uint256[] _cancellationFrom, uint256[] _cancellationTo, uint8[] _cancellationAmount, uint256 _total, uint256 _id );
+  event BookingAccepted(address _sender, uint256 id);
+  event BookingRejected(address _sender, uint256 id);
+
+  modifier onlyManager {
+        require(msg.sender == manager);
+        _;
+    }
+
   /**
    * @dev Constructor.
    * @param _manager address of hotel owner
    * @param _dataUri pointer to hotel data
    * @param _index originating WTIndex address
    */
-  constructor(address _manager, string _dataUri, address _index) public {
+  constructor (address _manager, string _dataUri, address _index) public {
     require(_manager != address(0));
     require(_index != address(0));
     require(bytes(_dataUri).length != 0);
@@ -39,6 +63,37 @@ contract Hotel is AbstractHotel {
   function _changeManagerImpl(address _newManager) internal {
     require(_newManager != address(0));
     manager = _newManager;
+  }
+  /**
+   * @dev Accepts booking request.
+   * @param id The id of the accepted booking request
+   */
+  function acceptBooking(uint256 id) public onlyManager{
+    require(bookingRequests[id].status == BookingRequestStatus.PENDING);
+    bookingRequests[id].status = BookingRequestStatus.ACCEPTED;
+    emit BookingAccepted(bookingRequests[id].sender, id);
+  }
+  /**
+   * @dev Rejects booking request.
+   * @param id The id of the rejected booking request
+   */
+  function rejectBooking(uint256 id) public onlyManager{
+    require(bookingRequests[id].status == BookingRequestStatus.PENDING);
+    bookingRequests[id].status = BookingRequestStatus.REJECTED;
+    emit BookingRejected(bookingRequests[id].sender, id);
+  }
+
+  /**
+   * @dev Adds booking request.
+   * @param info encrypted information about the booking
+   * @param cancellationFrom list containing start dates of cancellation policies
+   * @param cancellationTo list containing end dates of cancellation policies
+   * @param cancellationAmount list containing amounts of cancellation policies
+   */
+  function book(string info, uint256[] cancellationFrom, uint256[] cancellationTo, uint8[] cancellationAmount) public payable {
+    bookingRequests.push(BookingRequest(msg.sender, info, cancellationFrom, cancellationTo, cancellationAmount, msg.value, BookingRequestStatus.PENDING));
+    manager.transfer(msg.value);
+    emit newBookingRequest(msg.sender, info, cancellationFrom, cancellationTo, cancellationAmount, msg.value, bookingRequests.length-1);
   }
 
 }
